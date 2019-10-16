ec6fcf3fde54cfc38aa7b3bc59f0976de9b6f603
srujanam <srujanam@HDRVELOCITY34.Virtusa.com>
2019-10-16 12:52:20 +0530

    newly started

---
+++ B2CShoppersDen/src/com/b2cshoppersden/view/AdminLoginView.java
@@ -0,0 +1,10 @@
+package com.b2cshoppersden.view;
+
+public class AdminLoginView {
+
+	public void mainAdminView() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ B2CShoppersDen/src/com/b2cshoppersden/view/CustomerLoginView.java
@@ -0,0 +1,10 @@
+package com.b2cshoppersden.view;
+
+public class CustomerLoginView {
+
+	public void mainCustomerView() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
